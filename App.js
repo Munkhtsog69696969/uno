13647ca82ab439858e701e40b90a3d598ecf18ea
Munkhtsog69696969 <95847767+Munkhtsog69696969@users.noreply.github.com>
2022-11-29 19:34:26 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,128 @@
+import './App.css';
+
+function App() {
+  const totalCards=[
+    //blue cards 0-19
+    "blue-0",
+    "blue-1",
+    "blue-2",
+    "blue-3",
+    "blue-4",
+    "blue-5",
+    "blue-6",
+    "blue-7",
+    "blue-8",
+    "blue-9",
+    "blue-10",
+    "blue-11",
+    "blue-12",
+    "blue-13",
+    "blue-14",
+    "blue-15",
+    "blue-16",
+    "blue-17",
+    "blue-18",
+    "blue-19",
+    //green cards 0-19
+    "green-0",
+    "green-1",
+    "green-2",
+    "green-3",
+    "green-4",
+    "green-5",
+    "green-6",
+    "green-7",
+    "green-8",
+    "green-9",
+    "green-10",
+    "green-11",
+    "green-12",
+    "green-13",
+    "green-14",
+    "green-15",
+    "green-16",
+    "green-17",
+    "green-18",
+    "green-19",
+    //red cards 0-19
+    "red-0",
+    "red-1",
+    "red-2",
+    "red-3",
+    "red-4",
+    "red-5",
+    "red-6",
+    "red-7",
+    "red-8",
+    "red-9",
+    "red-10",
+    "red-11",
+    "red-12",
+    "red-13",
+    "red-14",
+    "red-15",
+    "red-16",
+    "red-17",
+    "red-18",
+    "red-19",
+    //yellow cards 0-19
+    "yellow-0",
+    "yellow-1",
+    "yellow-2",
+    "yellow-3",
+    "yellow-4",
+    "yellow-5",
+    "yellow-6",
+    "yellow-7",
+    "yellow-8",
+    "yellow-9",
+    "yellow-10",
+    "yellow-11",
+    "yellow-12",
+    "yellow-13",
+    "yellow-14",
+    "yellow-15",
+    "yellow-16",
+    "yellow-17",
+    "yellow-18",
+    "yellow-19",
+
+    //blue-skipCards
+    "blue-skipCard-1",
+    "blue-skipCard-2",
+    //green-skipCards
+    "green-skipCard-1",
+    "green-skipCard-2",
+    //red-skipCards
+    "red-skipCard-1",
+    "red-skipCard-2",
+    //yellow-skipCards
+    "yellow-skipCard-1",
+    "yellow-skipCard-2",
+    
+    //blue-reverseCard
+    "blue-reverseCard-1",
+    "blue-reverseCard-2",
+    //green-reverseCard
+    "green-reverseCard-1",
+    "green-reverseCard-2",
+    //red-reverseCard
+    "red-reverseCard-1",
+    "red-reverseCard-2",
+    //yellow-reverseCard
+    "yellow-reverseCard-1",
+    "yellow-reverseCard-2",
+  ];
+
+  totalCards.forEach((card,i)=>{
+    console.log(card,i+1)
+  })
+
+  return (
+    <>
+    uno
+    </>
+  );
+}
+
+export default App;
